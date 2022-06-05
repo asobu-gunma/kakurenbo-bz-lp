1ced3774029cd28dea3f57bd3cc2c4ecd82e2971
Yuta Toyokawa <kppg42@gmail.com>
2022-06-06 01:16:45 +0900

    フッターの初期実装

---
+++ components/global/Footer.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+v-footer(dark padless)
+  v-card.flex(flat tile)
+    v-card-text.teal.text-center.pb-0
+      v-btn(
+        v-for="text in navItems"
+        :key="text"
+        text
+      ) {{ text }}
+      v-btn(text)
+        span.me-1 ブログ
+        v-icon(dense) mdi-open-in-new
+    v-card-text.teal.text-center
+      v-btn(
+        v-for="icon in navIcons"
+        :key="icon"
+        icon
+        x-large
+      )
+        v-icon {{ icon }}
+    v-card-text.text-center.white--text.py-2
+      span &copy;
+      span {{ new Date().getFullYear() }} - 
+      strong かくれんぼ in ぐんま
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "nuxt-property-decorator"
+
+@Component({})
+export default class GlobalFooter extends Vue {
+  @Prop({ type: Array, required: true })
+  readonly navItems!: string[]
+
+  navIcons: string[] = [
+    'mdi-facebook',
+    'mdi-twitter',
+    'mdi-youtube',
+  ]
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,29 @@
 <template lang="pug">
 v-app
-  global-header
+  global-header(:navItems="navItems")
   v-main
     nuxt
+  global-footer(:navItems="navItems")
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "nuxt-property-decorator"
 import GlobalHeader from "~/components/global/Header.vue"
+import GlobalFooter from "~/components/global/Footer.vue"
 
 @Component({
   components: {
     GlobalHeader,
+    GlobalFooter,
   }
 })
-export default class DefaultLayout extends Vue {}
+export default class DefaultLayout extends Vue {
+  navItems: string[] = [
+    'スポかくとは？',
+    'ニュース',
+    'サービス',
+    '運営者情報',
+    'お問い合わせ',
+  ]
+}
 </script>
-
-<style lang="sass" scoped>
-.logo
-  height: 45px
-  width: auto
-</style>
